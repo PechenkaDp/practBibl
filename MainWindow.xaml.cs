2b6f8f0ac578bcf48a67a2a4dbcf855d174250bf
dimap <dimap@DESKTOP-4IVPGHB>
2023-05-30 23:24:13 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using practBibl;
+
+namespace practBibl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        string file;
+        string stroka;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stroka = TextVboxe.Text;
+            file = FileN.Text;
+            TextVboxe.Text = SerializeAndDes.Serialize("", TextVboxe.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            stroka = TextVboxe.Text;
+            file = FileN.Text;
+            TextVboxe.Text = SerializeAndDes.Deserialize<string>(TextVboxe.Text);
+
+        }
+    }
+}
